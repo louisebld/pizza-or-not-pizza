19fc152885a1779c9cb3456bf68381fd74d27c82
Tom thierry <tomthierry73@gmail.com>
2024-02-15 16:34:46 +0100

    init nuxt front

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+S
+#front-pizza-or-not-pizza/.gitignore,8/4/8465a480f4b79a928cf2fce07111f334281595ea
---
+++ front-pizza-or-not-pizza/components/Input.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <v-file-input
+            v-model="file"
+            accept="image/*"
+            label="Select an image"
+            @change="onFileChange"
+        ></v-file-input>
+    </div>
+</template>
