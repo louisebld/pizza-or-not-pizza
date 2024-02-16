5d38c7b36474166300e1653033173809c2f8260f
Louise <louiisebld@gmail.com>
2024-02-16 08:53:33 +0100

    api file

---
+++ README.md
@@ -0,0 +1,18 @@
+# Pizza or Not Pizza ?
+    Réalisé par Louise Bollard et Tom Thierry - M2 INFO 2024
+
+## Lancement de l'API 🚀
+
+Pour lancer l'API, il suffit de lancer la commande suivante :
+
+    pip install fastapi uvicorn python-multipart
+    uvicorn main:app --reload
+    
+## Utilisation de l'API 🍕
+
+L'API est accessible à l'adresse suivante : `http://localhost:8000`
+
+Les routes disponibles sont les suivantes :
+    /predict : Permet de prédire si une image est une pizza ou non (POST)
+
+
--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
+import shutil
 import requests
+from fastapi import FastAPI, UploadFile
 
 API_URL = "https://api-inference.huggingface.co/models/louisebld/pizza-or-not-pizza-model"
 token = "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"
@@ -10,5 +12,20 @@ def query(filename):
     response = requests.post(API_URL, headers=headers, data=data)
     return response.json()
 
-output = query("pizza.png")
-print(output)
+app = FastAPI()
+
+@app.get("/my-first-api")
+def hello():
+    return {"Hello world!"}
+
+@app.post("/predict")
+async def predict(file: UploadFile | None = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        with open("temp.png", "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        output = query("temp.png")
+        return output
+
+
